087b23f402735d6b94d1d5583cc96491e98aebe4
José Eduardo <josee.edumont@gmail.com>
2020-08-14 00:04:43 -0300

    Create user migration

---
+++ priv/repo/migrations/20200814025839_create_users.exs
@@ -0,0 +1,12 @@
+defmodule InstagramClone.Repo.Migrations.CreateUsers do
+  use Ecto.Migration
+
+  def change do
+    create_if_not_exists table(:users) do
+      add :name, :string
+      add :username, :string
+      add :password, :string
+      timestamps()
+    end
+  end
+end
